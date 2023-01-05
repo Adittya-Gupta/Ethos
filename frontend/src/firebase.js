e285e29beba57d38f51a5d56865c7bf796749997
Nova_Adi <95131750+Adittya-Gupta@users.noreply.github.com>
2023-01-05 16:56:48 +0530

    added firebase in react(incomplete)

---
+++ frontend/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC-l4uSAqCKu-IYpRqX3psDwo3Dn68XzaI",
+  authDomain: "ethos-website-98c85.firebaseapp.com",
+  projectId: "ethos-website-98c85",
+  storageBucket: "ethos-website-98c85.appspot.com",
+  messagingSenderId: "458849843741",
+  appId: "1:458849843741:web:84805fb05fe2dded6ea4f4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
